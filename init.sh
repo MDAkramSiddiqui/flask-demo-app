1fc85950dc3a61922934a88c9c53928357a64516
MDAkramSiddiqui <mohammadakram077@gmail.com>
2021-02-06 22:08:30 +0530

    bug fix

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pip3 install -r requirements.txt && \
-virualenv venv-flask && \
+virtualenv venv-flask && \
 . venv-flask/bin/activate && \
 pip3 install vwo-python-sdk && \
 pip3 install python-dotenv && \
